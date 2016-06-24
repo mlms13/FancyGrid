77ab7ac4ce21c771bcc60f97a7144db915a4e8dd
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-06-24 16:46:47 -0600

    minor improvements

--- demo/Main.hx
+++ demo/Main.hx
@@ -2,14 +2,16 @@ import fancy.Grid;
 
 class Main {
   static function main() {
+    var cellWidth = 50,
+        cellHeight = 30;
     var grid = new Grid(dots.Query.find(".my-fancy-grid-container"), ({
       render: function (row, col) return dots.Dom.create("div.cell", '$row, $col'),
-      vOffset: function (row) return 30 * row,
-      hOffset: function (col) return 120 * col,
-      columns: 100,
-      rows: 200,
-      vSize: function () return 200 * 30,
-      hSize: function () return 100 * 120
+      vOffset: function (row) return cellHeight * row,
+      hOffset: function (col) return cellWidth * col,
+      columns: 100000,
+      rows: 200000,
+      vSize: function () return 200 * cellHeight,
+      hSize: function () return 100 * cellWidth
     } : GridOptions));
 
     grid.goto(10, 10);
--- src/fancy/Grid.hx
+++ src/fancy/Grid.hx
@@ -10,8 +10,35 @@ typedef GridOptions = {
   columns: Int,
   rows: Int,
   vSize: Void -> Float,
-  hSize: Void -> Float
+  hSize: Void -> Float,
+  ?vExtraSize: Null<Float>,
+  ?hExtraSize: Null<Float>
 };
+
+class RefSize {
+  var vSize: Void -> Float;
+  var hSize: Void -> Float;
+  var vExtraSize: Float;
+  var hExtraSize: Float;
+
+  public var width(get, null): Float;
+  public var height(get, null): Float;
+  public var offsetTop(get, null): Float;
+  public var offsetLeft(get, null): Float;
+
+  public function new(vSize: Void -> Float, hSize: Void -> Float, vExtraSize: Null<Float>, hExtraSize: Null<Float>) {
+    this.vSize = vSize;
+    this.hSize = hSize;
+    this.vExtraSize = null != vExtraSize ? vExtraSize : 0.5;
+    this.hExtraSize = null != hExtraSize ? hExtraSize : 0.5;
+  }
+
+  function get_width() return hSize() + 2 * hSize() * hExtraSize;
+  function get_height() return vSize() + 2 * vSize() * vExtraSize;
+
+  function get_offsetTop() return - vSize() * vExtraSize;
+  function get_offsetLeft() return - hSize() * hExtraSize;
+}
 
 class Grid {
   var options : GridOptions;
@@ -19,8 +46,13 @@ class Grid {
   var floater : Element;
   var vScroller : Element;
 
+  var refSize: RefSize;
+
   public function new(parent : Element, options : GridOptions) {
     this.options = options;
+
+    refSize = new RefSize(options.vSize, options.hSize, options.vExtraSize, options.hExtraSize);
+
     floater = Dom.create("div.fg-floater", ["style" => "position:absolute;"]);
     vScroller = Dom.create("div.fg-vscroller", ["style" => "position:absolute;right:0"]);
     gridContainer = Dom.create("div.fg-container", ["style" => "position:relative;width:100%;height:100%;overflow:hidden"], [floater, vScroller]);
@@ -30,42 +62,86 @@ class Grid {
   }
 
   function onWheel(e : js.html.WheelEvent) {
+    // TODO
+    // conditionally insert row if moving down
+    // conditionally append row if moving up
+    // conditionally insert col if moving right
+    // conditionally append col if moving left
     moveFloater(-e.deltaX, -e.deltaY);
   }
 
-  function getContainerSize() {
+  function getContainerSize()
     return {
       width: gridContainer.getOuterWidth(),
       height: gridContainer.getOuterHeight()
     };
-  }
 
   function moveFloater(x : Float, y : Float) {
     moveFloaterTo(floater.offsetLeft + x, floater.offsetTop + y);
   }
 
   function moveFloaterTo(x : Float, y : Float) {
+    var size = getContainerSize(),
+        h = floater.getOuterHeight(),
+        w = floater.getOuterWidth();
+    if(y + h < size.height) {
+      if(h < size.height) {
+        y = 0;
+      } else {
+        y = size.height - h;
+      }
+    } else if(y > 0) {
+      y = 0;
+    }
+    if(x + w < size.width) {
+      if(w < size.width) {
+        x = 0;
+      } else {
+        x = size.width - w;
+      }
+    } else if(x > 0) {
+      x = 0;
+    }
+    if(y + h < size.height) {
+      if(h < size.height) {
+        y = 0;
+      } else {
+        y = size.height - h;
+      }
+    }
     floater.style.top = '${y}px';
     floater.style.left = '${x}px';
+    trace('floater pos ($x, $y)');
   }
 
   public function goto(row : Int, col : Int) {
-    moveFloaterTo(0, 0);
     floater.empty();
 
     var size = getContainerSize();
+    // constraint movement
+    if(row < 0) row = 0;
+    else if(row >= options.rows) row = options.rows - 1;
+    if(col < 0) col = 0;
+    else if(col >= options.columns) col = options.columns - 1;
 
-    // TODO: add some validation that `row` < `options.rows`, same for cols
+    var maxcol = options.columns;
+
     for (r in row...options.rows) {
       var tr = Dom.create("div.fg-row");
-      for (c in col...options.columns) {
+      floater.append(tr);
+      for (c in col...maxcol) {
         var content = options.render(r, c);
         var td = Dom.create("div.fg-cell", [content]);
         tr.append(td);
-        if (tr.getOuterWidth() > size.width) break;
+        trace(r, c, tr.getOuterWidth(), size.width);
+        if (maxcol == options.columns && tr.getOuterWidth() > size.width) {
+          maxcol = c + 1;
+          break;
+        };
       }
-      floater.append(tr);
       if (floater.getOuterHeight() > size.height) break;
     }
+
+    moveFloaterTo(0, 0);
   }
 }

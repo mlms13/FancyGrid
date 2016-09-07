61741826addb8abb8356d52142a5dfaf504ba9e5
Michael Martin-Smucker <mlmartin13@gmail.com>
2016-09-07 15:48:06 -0600

    Fix binary search for out-of-bound values

--- test/fancy/core/TestSearch.hx
+++ test/fancy/core/TestSearch.hx
@@ -9,13 +9,17 @@ class TestSearch {
   public function comp(expected: Int)
     return function(v) return thx.Ints.compare(v, expected);
 
+  public function comp2(a: Int)
+    return -1;
+
   public function testBinarySearch() {
     var max = 10;
     var values = thx.Ints.range(0, max + 1);
-    trace(values);
     for(v in values)
       Assert.equals(v, binary(0, max, comp(v)));
 
     Assert.equals(3, binary(max, 0, comp(3)));
+
+    Assert.equals(3, binary(0, 3, comp2));
   }
 }
